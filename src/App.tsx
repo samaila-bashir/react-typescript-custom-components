0e62fcbc0cd9f6ba23c133ed4aa502a85aa2e81e
samaila-bashir <developer.chatto@gmail.com>
2024-03-08 13:14:12 +0100

    created a generic form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { useRef } from 'react';
 import './App.css';
+import Button from './components/Button';
+import Form, { type FormHandle } from './components/Form';
 import Input from './components/Input';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  // const input = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+
+  const handleSave = (data: unknown) => {
+    const extractedData = data as { name: string; age: string };
+
+    customForm.current?.clear();
+
+    console.log(extractedData);
+  };
 
   return (
     <main>
-      <Input id="name" label="Name" ref={input} />
+      <Form onSave={handleSave} ref={customForm}>
+        <Input type="text" id="name" label="Name" />
+        <Input type="number" id="number" label="Age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,48 @@
+import {
+  type ComponentPropsWithoutRef,
+  type FormEvent,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from 'react';
+
+export type FormHandle = {
+  clear: () => void;
+};
+
+type FormProps = ComponentPropsWithoutRef<'form'> & {
+  onSave: (value: unknown) => void;
+};
+
+const Form = forwardRef<FormHandle, FormProps>(
+  ({ onSave, children, ...otherProps }, ref) => {
+    const form = useRef<HTMLFormElement>(null);
+
+    // useImperativeHandle: used for exposing a function to a parent component
+    useImperativeHandle(ref, () => {
+      return {
+        clear() {
+          console.log('CLEARING');
+          form.current?.reset();
+        },
+      };
+    });
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const formData = new FormData(event.currentTarget);
+      const data = Object.fromEntries(formData);
+
+      onSave(data);
+    };
+
+    return (
+      <form onSubmit={handleSubmit} {...otherProps} ref={form}>
+        {children}
+      </form>
+    );
+  }
+);
+
+export default Form;
