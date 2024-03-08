35577564fc3c994fda764b46c8be94c991d636fe
samaila-bashir <developer.chatto@gmail.com>
2024-03-08 04:40:51 +0100

    created a generic button and link component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+// import Input from './components/Input';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {/* <Input id="name" label="Name" />
+      <Input id="age" label="Age" type="number" /> */}
+
+      <Button>Button 1</Button>
+      <Button>Link 1</Button>
+    </main>
   );
 }
 
---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import { type ComponentPropsWithoutRef } from 'react';
+
+interface InputProps extends ComponentPropsWithoutRef<'input'> {
+  id: string;
+  label: string;
+}
+
+const Input: React.FC<InputProps> = ({ id, label, ...props }) => {
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} {...props} />
+    </p>
+  );
+};
+
+export default Input;
